b14bf89072a6acf4e4a0c5d2403e885bc353f168
elmen98 <93799871+elmen98@users.noreply.github.com>
2024-04-12 18:06:24 -0600

    Update index.js

--- src/layouts/dashboard/index.js
+++ src/layouts/dashboard/index.js
@@ -7,7 +7,8 @@ import SideBar from "../../pages/dashboard/SideBar";
 import { useDispatch, useSelector } from "react-redux";
 // import { socket } from "../../socket";
 import { socket, connectSocket } from "../../socket";
-import { showSnackbar } from "../../redux/slices/app";
+import { SelectConversation, showSnackbar } from "../../redux/slices/app";
+import { AddDirectConversation, AddDirectMessage, UpdateDirectConversation } from "../../redux/slices/conversation";
 
 const DashboardLayout = () => {
   const dispatch = useDispatch();
@@ -15,6 +16,10 @@ const DashboardLayout = () => {
   const { isLoggedIn } = useSelector((state) => state.auth);
 
   const user_id = window.localStorage.getItem("user_id");
+
+  const { conversations, current_conversation } = useSelector(
+    (state) => state.conversation.direct_chat
+  );
 
   useEffect(() => {
     if (isLoggedIn) {
@@ -50,14 +55,47 @@ const DashboardLayout = () => {
         dispatch(showSnackbar({ severity: "success", message: data.message }));
         
       });
+      socket.on("start_chat", (data) => {
+        console.log(data);
+        // add / update to conversation list
+        const existing_conversation = conversations.find(
+          (el) => el?.id === data._id
+        );
+        if (existing_conversation) {
+          // update direct conversation
+          dispatch(UpdateDirectConversation({ conversation: data }));
+        } else {
+          // add direct conversation
+          dispatch(AddDirectConversation({ conversation: data }));
+        }
+        dispatch(SelectConversation({ room_id: data._id }));
+      });
+
+      socket.on("new_message", (data) => {
+        const message = data.message;
+        console.log(current_conversation, data);
+        // check if msg we got is from currently selected conversation
+        if (current_conversation?.id === data.conversation_id) {
+          dispatch(
+            AddDirectMessage({
+              id: message._id,
+              type: "msg",
+              subtype: message.type,
+              message: message.text,
+              incoming: message.to === user_id,
+              outgoing: message.from === user_id,
+            })
+          );
+        }
+      });
     }
     // Remove event listener on component unmount
     return () => {
       socket?.off("new_friend_request");
       socket?.off("request_accepted");
       socket?.off("request_sent");
-      // socket?.off("start_chat");
-      // socket?.off("new_message");
+      socket?.off("start_chat");
+      socket?.off("new_message");
       // socket?.off("audio_call_notification");
     };
   }, [isLoggedIn, socket]);
