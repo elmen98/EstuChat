5f8ffcd5f0a1a6380391c0d7fe93002492c5c0ee
elmen98 <93799871+elmen98@users.noreply.github.com>
2024-05-06 21:15:53 -0600

    front vercel

--- Frontend/src/config.js
+++ Frontend/src/config.js
@@ -4,7 +4,7 @@ import { enUS, frFR, zhCN, viVN, arSD } from '@mui/material/locale';
 // routes
 import { PATH_DASHBOARD } from "./routes/paths";
 
-export const BASE_URL = "http://localhost:3001";
+export const BASE_URL = "https://estu-chat-api.vercel.app";
 
 export const defaultSettings = {
   themeMode: "light",
--- Frontend/src/socket.js
+++ Frontend/src/socket.js
@@ -3,7 +3,7 @@ import io from "socket.io-client";
 let socket;
 
 const connectSocket = (user_id) => {
-  socket = io("http://localhost:3001", {
+  socket = io("https://estu-chat-api.vercel.app", {
     query: `user_id${user_id}`,
   });
 };
