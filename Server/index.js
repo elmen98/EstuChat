a7c19dbdf28fa6e3a2c67b01f473795eb301e13f
elmen98 <93799871+elmen98@users.noreply.github.com>
2024-05-06 21:46:40 -0600

    Update index.js

--- Server/index.js
+++ Server/index.js
@@ -22,7 +22,7 @@ const OneToOneMessage = require("./models/OneToOneMessage");
 
 const io = new Server(server, {
   cors: {
-    origin: "https://estu-chat-frontend.vercel.app",
+    origin: "https://estu-chat-frontend.vercel.app/auth/login",
     methods: ["GET", "POST"],
     credentials: true
   },
